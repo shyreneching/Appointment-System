1bd008e16a3b1f2c607595163b40e8fd8da3a6fa
ShyreneChing <shyrene_ching@dlsu.edu.ph>
2019-10-12 20:05:49 +0800

    Edit, delete and view appointments

--- controller/secretaryController.js
+++ controller/secretaryController.js
@@ -6,6 +6,19 @@ router.get("/", (req, res) => {
     res.render("addappointment.hbs");
 })
 
+router.get("/appointmentlist", (req, res) => {
+    Appointment.find({}, (err, docs)=>{
+        if(err){
+            res.send(err)
+        }
+        else{
+            res.render("admin.hbs",{
+                appointments: docs
+            })
+        }
+    })
+})
+
 router.post("/create", (req, res) => {
 
     let patientname = req.body.pn
@@ -35,12 +48,57 @@ router.post("/create", (req, res) => {
     })
 })
 
-router.post("/update", (req, res) => {
+router.get("/edit", (req, res)=>{
+    //just find the user given the id
+    console.log("GET /edit" + req.query.id)
+    Appointment.findOne({
+        _id: req.query.id
 
+    }, (err, doc)=>{
+        if(err){
+            res.send(err)
+        }else{
+            console.log(doc)
+            //send all the details of the user to edit.hbs
+            res.render("edit.hbs", {
+                appointment: doc
+            })
+        }
+    })
+})
+
+
+router.post("/update", (req, res) => {
+    Appointment.update({
+        _id: req.body.id
+    }, {
+        patientname:  req.body.pn,
+        patientcontact: req.body.pc,
+        process: req.body.pp,
+        notes: req.body.note,
+        time: req.body.time,
+        date: req.body.date,
+        doctor: req.body.doc
+    }, (err, doc)=>{
+       if(err){
+           res.send(err)
+       }else{
+           res.redirect("/")
+       }
+    })
 })
 
 router.post("/delete", (req, res) => {
-
+    Appointment.deleteOne({
+        _id: req.body.id
+    }, (err, doc)=>{
+       if(err){
+           res.send(err)
+       }else{
+           //res.redirect("/users")
+           res.send(doc)
+       }
+    })
 })
 
 module.exports = router;
