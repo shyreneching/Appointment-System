0ae78a1be001dc18f67ef5eebeca110176a706a9
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-10-18 02:16:33 +0800

    commafied more than one doctor/ process

--- public/javascript/handlebar_extras.js
+++ public/javascript/handlebar_extras.js
@@ -3,9 +3,15 @@
 */
 
 Handlebars.registerHelper('colorcells', function(index) {
-    console.log(index);
     let colorsArrays = ["pink","blue", "orange", "green", "purple", "olive", "yellow", "teal"];
     let num = index % colorsArrays.length;
-    console.log(colorsArrays[num]);
     return new Handlebars.SafeString(colorsArrays[num]);
+});
+
+Handlebars.registerHelper('commafy', function(index) {
+    if (index > 0){
+        return new Handlebars.SafeString(", ");
+    } else {
+        return new Handlebars.SafeString("");
+    }
 });
