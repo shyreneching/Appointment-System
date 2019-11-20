a27563fb51b4e2fc28c50af0da5d0e2756ad643d
stanls <stanley_sie@dlsu.edu.ph>
2019-11-21 00:18:11 +0800

    Update adminController.js

--- controller/adminController.js
+++ controller/adminController.js
@@ -72,7 +72,7 @@ router.post("/deleteAccount", async (req, res) => {
         Doctor.delete(account.doctorID);
         let appointments = await Appointment.getDoctorAppointment(account.doctorID);
         for (var i = 0; i < appointments.length; i++) {
-            let appID = appointments[i]._id;
+            let appID = appointments[i];
             if(appID.doctor == "" || appID.doctor.length == 0) {
                 await Appointment.delete(appID);
             }
